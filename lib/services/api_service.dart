9461b54d5424bb506748df32f5c42c9f8f39eb66
Siratheenext <siratheenext@gmail.com>
2025-04-21 06:04:47 +0700

    Set Default Emulator

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,10 +3,10 @@ import 'package:http/http.dart' as http;
 import '../models/product_model.dart';
 
 Future<List<Product>> fetchProducts() async {
-  // final response = await http.get(Uri.parse('http://10.0.2.2:3000/products'));
-  final response = await http.get(
-    Uri.parse('http://172.20.10.4:3000/products'),
-  ); //check ipconfig for use ipv4 and connect same internet
+  final response = await http.get(Uri.parse('http://10.0.2.2:3000/products'));
+  // final response = await http.get(
+  //   Uri.parse('http://172.20.10.4:3000/products'),
+  // ); //check ipconfig for use ipv4 and connect same internet
 
   if (response.statusCode == 200) {
     List<dynamic> data = jsonDecode(response.body);
